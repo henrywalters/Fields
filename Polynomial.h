c20feeeaf5311e5154d8bd8718a1793b257087b7
henrywalters <henrywalters20@gmail.com>
2016-04-03 12:27:23 -0400

    Create Polynomial.h

---
+++ Polynomial.h
@@ -0,0 +1,28 @@
+#ifndef POLYNOMIAL_H
+#define POLYNOMIAL_H
+#include <vector>
+
+class Polynomial
+{
+    public:
+        int degree = 0;
+        std::vector<float> coef;
+        Polynomial();
+        virtual ~Polynomial();
+        void set_degree(int);
+        void define();
+        void display();
+
+
+        Polynomial operator/(const Polynomial&);
+        Polynomial operator%(const Polynomial&);
+        Polynomial operator*(const Polynomial&);
+        Polynomial operator~();
+        Polynomial operator+(const Polynomial&);
+        Polynomial operator-(const Polynomial&);
+
+    protected:
+    private:
+};
+
+#endif // POLYNOMIAL_H
